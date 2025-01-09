865ce4c19934c55c01246e5ccbed67fad5475223
SalmanSamiKhan <salmansamiikhan@gmail.com>
2025-01-10 02:27:16 +0600

    simple syntax

--- dna_toolkit.py
+++ dna_toolkit.py
@@ -2,6 +2,8 @@ from collections import Counter
 # nucloetides set
 nucleotides = ["A", "C", "G", "T"]
 
+
+'''
 # validate dna sequence
 def validate_sequence(dna_sequence):
     return dna_sequence if set(dna_sequence.upper()).issubset(nucleotides) else False
@@ -9,4 +11,21 @@ def validate_sequence(dna_sequence):
 
 def count_nucleotides(dna_sequence):
     # count nucleotides
-    return Counter(dna_sequence)
+    return dict(Counter(dna_sequence))
+'''
+
+# validate dna sequence
+def validate_sequence(dna_sequence):
+    dna_sequence = dna_sequence.upper()
+    for nucleotide in dna_sequence:
+        if nucleotide not in nucleotides:
+            return False
+    return dna_sequence
+
+
+def count_nucleotides(dna_sequence):
+    # count nucleotides
+    dna_seq_dict = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
+    for nucleotide in dna_sequence:
+        dna_seq_dict[nucleotide] += 1
+    return dna_seq_dict
--- main.py
+++ main.py
@@ -7,8 +7,9 @@ rnd_dna_str = ''.join(random.choices(nucleotides, k=50))
 
 # validate sequence
 valid_dna_sequence = validate_sequence(rnd_dna_str)
-print(valid_dna_sequence)
+print('valid dna sequence: ',valid_dna_sequence)
 
 # count nucleotides
-nucleotide_counts = count_nucleotides(valid_dna_sequence)
-print(nucleotide_counts)
+nucleotide_dict = count_nucleotides(valid_dna_sequence)
+print('nucleotide dict: ',nucleotide_dict)
+print('nucleotide values: ',*nucleotide_dict.values())
